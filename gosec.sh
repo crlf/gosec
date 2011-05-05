fbed4900d614bbaeff2e8681226f28e58f67d088
Rishi Narang <code@wtfuzz.com>
2011-05-05 08:05:04 -0700

    Revision and Edits

--- gosec.sh
+++ gosec.sh
@@ -30,23 +30,29 @@ SEARCHF=`echo $SEARCH | sed -e 's/ /./g'`
 URL="http://google.com/search?hl=en&safe=off&q="
 STRING=`echo $SEARCH | sed 's/ /%20/g'`
 URI="$URL%22$STRING%22"
-    
-lynx -dump $URI > gone.tmp
+
+lynx -dump $URI > g1.tmp
 if [ $? -gt 0 ]
 then
  echo "ERROR: Execution error. Check if 'lynx' package is installed."
  exit 127
 fi
-sed 's/http/\^http/g' gone.tmp | tr -s "^" "\n" | grep http| sed 's/\ .*//g' > gtwo.tmp
-rm gone.tmp
-sed '/google.com/d' gtwo.tmp > URLS_$SEARCHF
-rm gtwo.tmp
-    
-echo "SUCCESS: Extracted '`wc -l URLS_$SEARCHF | awk '{print $1}'` links' and listed them in '`pwd`/URLS_$SEARCHF' file for reference."
-echo ""
-echo "-------------------------------- URL RESULTS --------------------------------"
+sed 's/http/\^http/g' g1.tmp | tr -s "^" "\n" | grep http| sed 's/\ .*//g' > g2.tmp
+sed '/google.com/d; /webcache/d' g2.tmp > URLS
+
+echo "" > URLS_$SEARCHF
+echo "------------------------------- URL RESULTS --------------------------------" >> URLS_$SEARCHF
+echo "" >> URLS_$SEARCHF
+sed = URLS | sed 'N;s/\n/. /' >> URLS_$SEARCHF
+echo "" >> URLS_$SEARCHF
+echo "----------------------------------------------------------------------------" >> URLS_$SEARCHF
+rm g1.tmp g2.tmp URLS
+
+NUM=`wc -l URLS_$SEARCHF | awk '{print $1}'`
+LOC=`expr $NUM - 5`
+
+echo "SUCCESS: Extracted '$LOC links' and listed them in '`pwd`/URLS_$SEARCHF' file for reference."
 cat URLS_$SEARCHF
-echo "-----------------------------------------------------------------------------"
 echo ""
 
 #EOF
